45ce1751d31b15c7da329ac1be954f9788f8fef2
Achintya Joy <achintya.joy99@gmail.com>
2025-11-14 13:42:18 -0500

    cta_pipeline: constants: add reddit configuration

---
+++ cta_pipeline/constants.py
@@ -0,0 +1,19 @@
+"""Constants used throughout the CTA pipeline."""
+
+# File paths - Raw data (output from fetch scripts)
+RAW_DATA_DIR_REDDIT = "data/posts/reddit"
+POSTS_PATH_REDDIT = "data/posts/reddit/reddit_posts.csv"
+COMMENTS_PATH_REDDIT = "data/posts/reddit/reddit_comments.csv"
+
+# Reddit subreddits to fetch from
+REDDIT_SUBREDDITS = [
+    "Chicago",
+    "AskChicago",
+    "CarFreeChicago",
+    "AskCHI",
+    "cta",
+    "ChicagoUrbanism",
+    "WindyCity",
+    "ChicagoNWSide",
+    "greatNWSide",
+]
